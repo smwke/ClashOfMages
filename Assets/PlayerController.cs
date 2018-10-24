3b44777ccf4ad080d159902336ad6bcb5f4a0fa0
smwke <44174027+smwke@users.noreply.github.com>
2018-10-24 19:59:52 +0300

    Added basic animations

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,75 +4,101 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Unit thisUnit;
 
     [SerializeField]
-    float moveSpeed = 4f, rotSpeed = 10f;
-
-    [SerializeField]
-    Transform camera;
+    Transform _camera;
 
     CharacterController controller;
     Animator anim;
 
     float vInput, hInput;
-    bool isWalking = false;
+    bool isWalking = false, isAttacking = false;
 
-    // Use this for initialization
     void Start()
     {
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+        thisUnit = GetComponent<Unit>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         vInput = Input.GetAxisRaw("Vertical");
         hInput = Input.GetAxisRaw("Horizontal");
 
-        Rotate();
+        if (Input.GetMouseButton(0) && !isAttacking) Attack(); 
 
+        Rotate(); // get rotation vector and set the walking bool to true if we are moving /
+        Move(); // moves the character according to previous booleans /
+
+
+
+        ///if (isWalking) controller.Move(transform.forward * Time.deltaTime * moveSpeed); /
+    }
+    void Attack()
+    {
+        isAttacking = true;
+        anim.SetBool("Attack", isAttacking);
+        anim.SetLayerWeight(1, 1);
+    }
+    public void AttackDone()
+    {
+        isAttacking = false;
+        anim.SetBool("Attack", isAttacking);
+        anim.SetLayerWeight(1, Mathf.Lerp(1,0,Time.deltaTime));
+        
+    }
+    void Move()
+    {
         anim.SetBool("Walk", isWalking);
-        anim.SetBool("Sprint", Input.GetKey(KeyCode.LeftShift));
 
-        if (isWalking) controller.Move(transform.forward * Time.deltaTime * moveSpeed);
+        if (Input.GetKey(KeyCode.LeftShift) && !isAttacking)
+        {
+            anim.SetBool("Sprint", true);
+            if (isWalking) controller.Move(transform.forward * Time.deltaTime * thisUnit.sprintSpeed);
+        }
+        else
+        {
+            anim.SetBool("Sprint", false);
+            if (isWalking) controller.Move(transform.forward * Time.deltaTime * thisUnit.moveSpeed);
+        }
     }
     void Rotate()
     {
+        float _rotSpeed = thisUnit.rotSpeed * Time.deltaTime;
 
-        float _rotSpeed = rotSpeed * Time.deltaTime;
-
-        if (vInput > 0 && hInput == 0)// Forward movement
+        if (vInput > 0 && hInput == 0)// Forward movement /
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, camera.eulerAngles.y, 0f), _rotSpeed); isWalking = true;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, _camera.eulerAngles.y, 0f), _rotSpeed); isWalking = true;
         }
-        else if (vInput > 0 && hInput > 0)//Forward-right movement
+        else if (vInput > 0 && hInput > 0)//Forward-right movement /
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, camera.eulerAngles.y + 45, 0f), _rotSpeed); isWalking = true;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, _camera.eulerAngles.y + 45, 0f), _rotSpeed); isWalking = true;
         }
-        else if (vInput > 0 && hInput < 0)//Forward-left movement
+        else if (vInput > 0 && hInput < 0)//Forward-left movement /
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, camera.eulerAngles.y - 45, 0f), _rotSpeed); isWalking = true;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, _camera.eulerAngles.y - 45, 0f), _rotSpeed); isWalking = true;
         }
-        else if (vInput == 0 && hInput > 0)//Right movement
+        else if (vInput == 0 && hInput > 0)//Right movement /
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, camera.eulerAngles.y + 90, 0f), _rotSpeed); isWalking = true;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, _camera.eulerAngles.y + 90, 0f), _rotSpeed); isWalking = true;
         }
-        else if (vInput == 0 && hInput < 0)//Left movement
+        else if (vInput == 0 && hInput < 0)//Left movement /
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, camera.eulerAngles.y - 90, 0f), _rotSpeed); isWalking = true;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, _camera.eulerAngles.y - 90, 0f), _rotSpeed); isWalking = true;
         }
-        else if (vInput < 0 && hInput == 0)//Back movement
+        else if (vInput < 0 && hInput == 0)//Back movement /
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, camera.eulerAngles.y + 180, 0f), _rotSpeed); isWalking = true;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, _camera.eulerAngles.y + 180, 0f), _rotSpeed); isWalking = true;
         }
-        else if (vInput < 0 && hInput > 0)//Back-right movement
+        else if (vInput < 0 && hInput > 0)//Back-right movement /
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, camera.eulerAngles.y + 135, 0f), _rotSpeed); isWalking = true;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, _camera.eulerAngles.y + 135, 0f), _rotSpeed); isWalking = true;
         }
-        else if (vInput < 0 && hInput < 0)//Back-left movement
+        else if (vInput < 0 && hInput < 0)//Back-left movement /
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, camera.eulerAngles.y - 135, 0f), _rotSpeed); isWalking = true;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, _camera.eulerAngles.y - 135, 0f), _rotSpeed); isWalking = true;
         }
         else
         {
--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -3,14 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    int Health;
+    public float moveSpeed = 2.5f, sprintSpeed = 5f, rotSpeed = 10f;
 }
