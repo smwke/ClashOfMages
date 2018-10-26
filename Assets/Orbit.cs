37ecd1aef8d0d8f9155d51d162044279f48e5da3
smwke <smwke@users.noreply.github.com>
2018-10-26 20:15:40 +0300

    Changed Ball Follow

--- Assets/Orbit.cs
+++ Assets/Orbit.cs
@@ -5,47 +5,19 @@ using UnityEngine;
 public class Orbit : MonoBehaviour
 {
     [SerializeField]
-    Transform target;
-    [SerializeField]
-    float speed = 50f;
-
-   
-    ConfigurableJoint joint;
+    Transform target; float speed = 100f;
 
     [SerializeField]
     float rotspeed = 1f;
-    float timer = 0f;
-    bool updown = true;
     private void Start()
     {
-        joint = GetComponent<ConfigurableJoint>();
     }
     void LateUpdate()
     {
-        timer += Time.deltaTime;
+        transform.parent = null;
+
+        transform.position = Vector3.Lerp(transform.position,target.position - target.forward*2,rotspeed * Time.deltaTime);
         
-        transform.RotateAround(target.position, Vector3.up, speed * Time.deltaTime);
-        joint.targetPosition = transform.position;
-
-        Spring();
-    }
-    void Spring()
-    {
-        if ( timer >= 0.5 && updown) {
-            //transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, 1, transform.position.z), rotspeed * Time.deltaTime);
-            joint.targetPosition = new Vector3(transform.position.x,1,transform.position.z);
-            timer = 0f;
-            updown = false;
-        }
-
-        if (timer >= 0.5 && !updown)
-        {
-            //transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, -1, transform.position.z), rotspeed * Time.deltaTime);
-            joint.targetPosition = new Vector3(transform.position.x, -1, transform.position.z);
-            timer = 0f;
-            updown = true;
-        }
-
     }
 
 }
