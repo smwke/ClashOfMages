9c7f0196dbf113aa5be7c2f7c951d4b75176c395
smwke <smwke@users.noreply.github.com>
2018-10-26 20:50:47 +0300

    Update Assets/Unit.cs

--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour {
     [SerializeField]
-    int Health;
+    float Health;
     public float moveSpeed = 2.5f, sprintSpeed = 5f, rotSpeed = 10f;
+
+    void TakeDamage(float damage)
+    {
+        if (Health - damage <= 0) Die();
+        else Health -= damage;
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
 }
